546b692cf75b3363d31fbc6b0afbd4dbba4b6bac
HUANG37W <huang78@byui.edu>
2023-05-23 16:25:26 -0600

    update car database

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,15 +2,21 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
+  try{
   console.log("getAll");
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
-};
+} catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err });
+}
+}
 
 const getSingle = async (req, res, next) => {
+  try{
   console.log("getSingle");
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
@@ -22,15 +28,24 @@ const getSingle = async (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
-};
+} catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err });
+}
+}
 
 const createContact = async (req, res, next) => {
+  try{
   const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    Name: req.body.Name,
+    Miles_per_Gallon: req.body.Miles_per_Gallon,
+    Cylinders: req.body.Cylinders,
+    Displacement: req.body.Displacement,
+    Horsepower: req.body.Horsepower,
+    Weight_in_lbs: req.body.Weight_in_lbs,
+    Acceleration: req.body.Acceleration,
+    Year: req.body.Year,
+    Origin: req.body.Origin
   };
   const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
   if (response.acknowledged) {
@@ -38,17 +53,27 @@ const createContact = async (req, res, next) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
-};
+} catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err });
+}
+}
+
 
 const updateContact = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   // be aware of updateOne if you only want to update specific fields
   const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    Name: req.body.Name,
+    Miles_per_Gallon: req.body.Miles_per_Gallon,
+    Cylinders: req.body.Cylinders,
+    Displacement: req.body.Displacement,
+    Horsepower: req.body.Horsepower,
+    Weight_in_lbs: req.body.Weight_in_lbs,
+    Acceleration: req.body.Acceleration,
+    Year: req.body.Year,
+    Origin: req.body.Origin
   };
   const response = await mongodb
     .getDb()
@@ -61,9 +86,14 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
-};
+} catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err });
+}
+}
 
 const deleteContact = async (req, res) => {
+  try{
   const userId = new ObjectId(req.params.id);
   const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
   console.log(response);
@@ -72,7 +102,11 @@ const deleteContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
-};
+} catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err });
+}
+}
 
 
 module.exports = { getAll, getSingle, createContact, updateContact, deleteContact};
